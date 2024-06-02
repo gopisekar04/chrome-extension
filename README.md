c0d95a8315f0fd24c73149cbf5388de043f664fa
Gopi <itsgopiofficial@gmail.com>
2024-06-02 18:35:48 +0530

    chrome extension + gemini api

---
+++ README.md
@@ -0,0 +1,11 @@
+## Simple Chrome Extension with Gemini API
+
+Just Simple Chrome Extension Example Project, using Gemini API from Google<br>
+Because Chrome Extension has strict security policy, Gemini REST API is used instead of Module API.<br>
+You can simply input some texts, and get answer from Gemini with Extension.
+
+### Project Structure
+
+`maniest.json` is Project Property File.<br>
+`popup_menu.html` is Simple Layout with HTML Elements<br>
+`popup_menu.js` is Main Logic of Extension. You need to enter Gemini API Key from [AI Studio](https://aistudio.google.com) at `GEMINI_API_KEY` Variable.
---
+++ popup_menu.js
@@ -0,0 +1,32 @@
+const GEMINI_API_KEY = "AIzaSyCoDtgiWUUrnw5XIngXEA2zsi4kKfWcJAA";
+const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GEMINI_API_KEY}`;
+
+let btnGemini = document.getElementById("btn_gemini");
+let inputText = document.getElementById("input_text");
+let resultText = document.getElementById("result_text");
+
+btnGemini.addEventListener("click", () => {
+    btnGemini.disabled = true;
+
+    fetch(GEMINI_API_URL, {
+        method: "POST",
+        body: JSON.stringify({
+            contents: [{
+                parts:[{
+                    text: `hey gemini ai, once you calculate the answer please to review ur answer for any mistakes and make sure to provide the correct answers always. Also respond with the following topis.
+                    1. formula to solve the question
+                    2. hint
+                    3. final answer
+                    here is a question for you to solve ${inputText.value}`
+                }]
+            }]
+        }),
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then((result) => {
+        btnGemini.disabled = false;
+        resultText.innerText = result["candidates"][0]["content"]["parts"][0]["text"];
+    });
+});
